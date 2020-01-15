f889901781398b1c882831ed372923c29d1d4693
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-01-15 17:09:43 -0500

    proof of work for the client side

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,12 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    proof = 0
+    print("Proof of work start")
+    while valid_proof(block, proof) is False:
+        proof += 1
+    print("Proof of work end")
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +32,9 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f"{block_string}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
