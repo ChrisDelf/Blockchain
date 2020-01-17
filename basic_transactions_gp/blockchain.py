4c32fe5c8dcb191142328f950b188531b2aeb4de
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-01-16 19:04:38 -0500

    fixed cors issue

--- basic_transactions_gp/blockchain.py
+++ basic_transactions_gp/blockchain.py
@@ -3,6 +3,8 @@ import json
 from time import time
 from uuid import uuid4
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
@@ -115,6 +117,7 @@ class Blockchain(object):
 
 # Instantiate our Node
 app = Flask(__name__)
+CORS(app)
 
 # Generate a globally unique address for this node
 node_identifier = str(uuid4()).replace('-', '')
